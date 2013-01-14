8781a325042a985ace86ba74be2658e1a1ecde20
chethan <chethan@chethan.(none)>
2013-01-14 13:30:14 +0530

    Added code to use alsa lib and figure out the number of sound cards on the system

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <alsa/asoundlib.h>
 using namespace std;
+
+void countCards(){
+	int err, cardnum, totalCards = 0;
+	cardnum = -1;
+	while(1){
+		if((err = snd_card_next(&cardnum)) < 0){
+			cout<<"Error while fetching next sound card"<<endl;
+			break;
+		}
+		if(cardnum < 0) break;		
+		++totalCards;		
+	}
+	cout<<"We found "<<totalCards<<" on this system!"<<endl;
+}
+
 int main(){
     cout<<"Noise cancellation program";
+    countCards();
     return 0;
 }
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 CC = g++
 
 all: compile
-	$(CC) obj/*.o  -o nullspace
+	$(CC) obj/*.o -lasound -o nullspace 
 
 compile:
 	find -name '*.cpp' -print0 | xargs -0 $(CC) -c $(CPPFLAGS) && mkdir -p obj && mv *.o ./obj/
